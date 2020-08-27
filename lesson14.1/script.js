add5f911d0260efc295c426441a63a2f4c349c97
Artur Romanov <romanovjg@yandex.ru>
2020-08-27 12:34:34 +0300

    14.1 style modernisation

--- lesson14.1/script.js
+++ lesson14.1/script.js
@@ -13,21 +13,21 @@ DomElement.prototype.method=function(){
         if(this.selector.startsWith('.')){
             let elementDiv = document.createElement('div');
             elementDiv.className=this.selector.substring(1);
-            elementDiv.style.cssText=`height: ${this.height}; width: ${this.width}; background: ${this.bg}`;
-            elementDiv.style.fontSize=this.fontSize;
+            elementDiv.style.cssText=`height:`+ this.height +
+             `px; width:`+ this.width+`px; background-color:`+ this.bg + `;fontSize:`+this.fontSize+`px;`;
             elementDiv.textContent=this.selector.substring(1);
             doc.append(elementDiv);
             
         }else if(this.selector.startsWith('#')){
             let elementParag = document.createElement('p');
             elementParag.setAttribute('id',this.selector.substring(1));
-            elementParag.style.cssText=`height: ${this.height}; width: ${this.width}; background: ${this.bg}`;
-            elementParag.style.fontSize=this.fontSize;
+            elementParag.style.cssText=`height:`+ this.height +
+             `px; width:`+ this.width+`px; background-color:`+ this.bg + `;fontSize:`+this.fontSize+`px;`;
             elementParag.textContent=this.selector.substring(1);
             doc.append(elementParag);
         }
     
 };
 
-let test = new DomElement('#заthisни','300px','300px','pink','large');
+let test = new DomElement('#заthisни','300','300','pink','55');
 test.method();
