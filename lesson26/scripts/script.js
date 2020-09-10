084f3e4d5b36baf9a8842c4f7695ce37cc7ec6fd
Artur Romanov <romanovjg@yandex.ru>
2020-09-10 20:49:18 +0300

    initial commit

--- lesson26/scripts/script.js
+++ lesson26/scripts/script.js
@@ -359,4 +359,102 @@ const calc=(price=100)=>{
     }
 };
 calc(100);
+
+// send ajax-form
+const sendForm =()=>{
+    const errorMessage = 'Что-то пошло не так...',
+        loadMessage = 'Загрузка...',
+        successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+
+    forms(document.getElementById('form1'));
+    forms(document.getElementById('form2'));
+    forms(document.getElementById('form3'));
+
+    const statusMessage = document.createElement('div');
+    statusMessage.style.cssText='font-size: 2rem; color: white;';
+        
+        //Функция для всех форм, по item вешается обработчик событий на каждую из них
+    function forms(item){ 
+
+        let inputsArr = [...item.elements].filter(item => {
+            return item.tagName.toLowerCase() !== 'button' &&
+            item.type !== 'button'; 
+        });
+
+        //Валидация инпутов
+        inputsArr.forEach((item)=>{
+            item.addEventListener('input',()=>{
+                
+                if(item.name === 'user_phone' ){
+
+                    item.value=item.value.replace(/[^0-9\+]/, '');
+
+                }else if(item.name === 'user_name'|| item.name === 'user_message'){
+
+                    item.value=item.value.replace(/[^а-яё ]/gi,'');
+
+                }else if(item.name === 'user_email'){
+
+                    item.value=item.value.replace(/[^a-z0-9\.@\.\_]/gi,'');
+
+                }
+            });
+        });
+
+
+        item.addEventListener('submit', (event) => {
+            event.preventDefault();
+            item.append(statusMessage);
+            statusMessage.textContent = loadMessage;
+
+            const formData = new FormData(item);
+            let body = {};
+           
+            formData.forEach((val,key)=>{
+                body[key]=val;
+            });
+
+            postData(body,
+            ()=>{
+                statusMessage.textContent = successMessage;
+            },
+            (error)=>{
+                statusMessage.textContent = errorMessage;
+                console.error(error);
+            });
+
+            //Сброс инпутов
+            inputsArr.forEach((item)=>{
+                item.value='';
+            });
+    });
+    }
+
+    const postData = (body, outputData, errorData) => {
+
+        const request = new XMLHttpRequest();
+
+        request.addEventListener('readystatechange', ()=>{
+            if(request.readyState !== 4){
+                return;
+            }
+
+            if(request.status === 200){
+                outputData();
+            } else {
+                errorData(request.status);
+            }
+        });
+
+        request.open('post', './server.php');
+
+        request.setRequestHeader('Content-Type', 'application/json');
+       
+        request.send(JSON.stringify(body));
+
+    };
+
+};
+sendForm();
+
 });
